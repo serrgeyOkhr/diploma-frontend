31e5e5a321f68663db468def53d813d8b26e2ac0
Sergey <ohryamkin.ser@yandex.ru>
2022-03-11 14:00:03 +0700

    selected UI

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
+import naive from 'naive-ui'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(naive).use(store).use(router).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+      <n-button
+      color="#f0f0f0"
+      text-color="000000"
+      @click="showModal = true"
+      >naive-ui</n-button>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- modal from here -->
+      <n-modal v-model:show="showModal" preset="dialog" title="Dialog">
+        <template #header>
+          <div>title</div>
+        </template>
+        <div>content</div>
+        <template #action>
+          <div>action</div>
+        </template>
+      </n-modal>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+// import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue'
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  setup () {
+    return {
+      showModal: ref(false)
+    }
   }
 }
 </script>
