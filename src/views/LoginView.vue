16a0380f7d01141c815259b4be30a6ca2b8674e3
Sergey <ohryamkin.ser@yandex.ru>
2022-04-18 11:17:38 +0700

    LoginPage test interface

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -52,6 +52,7 @@
 </template>
 
 <script>
+/* eslint-disable no-unused-vars */
 // @ is an alias to /src
 // import ModalLogin from '@/components/ModalLogin.vue'
 import { ref } from 'vue'
@@ -65,10 +66,14 @@ export default {
     // ModalLogin
   },
   setup () {
+    const loginUrl = 'http://127.0.0.1:5000/login'
     const message = useMessage()
     const store = useStore()
     const router = useRouter()
     const formRef = ref(null)
+    const resp = ref(null)
+    const customError = ref(null)
+    const loading = ref(null)
     const formValue = ref({
       login: '',
       password: ''
@@ -85,32 +90,81 @@ export default {
         trigger: ['input', 'blur']
       }
     })
-    function checkUser (data) {
-      const dataToServer = {
+    function getUser (data) {
+      /* ожидаемый return: {  responce: xxx (type: Number),
+                              user: {
+                                name: 'Jon' (type: String),
+                                type: 1 / 2 / 69 {type: Number},
+                                group: 'IT-6969' / undefined (type: String or undefined)
+                              }
+                            }
+      */
+      const body = {
         login: data.value.login,
         password: data.value.password
       }
-      console.log(dataToServer)
+      loading.value = true
+      fetch(loginUrl, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+      })
+        .then(async (response) => { resp.value = await response.json() })
+        .then((response) => { loading.value = false })
+        .catch((error) => {
+          customError.value = error
+          setTimeout(() => { loading.value = false }, 1000)
+        })
+      store.commit('updateUser', resp.value.user)
+      return resp.value
+    }
+
+    function getStaticUser (data) {
       const Result = {
         response: 200,
-        // user: {
-        //   name: 'Антон Денисович',
-        //   type: 2,
-        //   group: undefined
-        // }
+        user: {
+          name: 'Сергей Вячеславович',
+          type: 1,
+          group: 'ИТ-181'
+        }
+      }
+      store.commit('updateUser', Result.user)
+      return Result
+    }
+    function getStaticProf (data) {
+      const Result = {
+        response: 200,
+        user: {
+          name: 'Антон Денисович',
+          type: 2,
+          group: undefined
+        }
+      }
+      store.commit('updateUser', Result.user)
+      return Result
+    }
+
+    function getStaticAdmin (data) {
+      const Result = {
+        response: 200,
         user: {
           name: 'Админ Админович',
           type: 69,
           group: undefined
         }
-        // user: {
-        //   name: 'Сергей Вячеславович',
-        //   type: 1,
-        //   group: 'ИТ-181'
-        // }
       }
       store.commit('updateUser', Result.user)
-      // localStorage.setItem('User', JSON.stringify(store.state.user))
+      return Result
+    }
+    function getStaticErrorUser (data) {
+      const Result = {
+        response: 400,
+        user: undefined
+      }
+      // store.commit('updateUser', Result.user)
       return Result
     }
 
@@ -118,16 +172,18 @@ export default {
       formRef,
       formValue,
       rules,
+      loading,
       handleValidateClick (e) {
         e.preventDefault()
         formRef.value?.validate((errors) => {
-          if (errors) {
-            console.log(errors)
-          } else {
-            const rez = checkUser(formValue)
+          if (!errors) {
+            // const rez = getUser(formValue)
+            const rez = getStaticUser(formValue)
+            // const rez = getStaticProf(formValue)
+            // const rez = getStaticAdmin(formValue)
+            // const rez = getStaticErrorUser(formValue)
             if (rez.response === 200) {
               message.info('Здравствуй, ' + rez.user.name)
-              console.log('hello')
               router.push({ path: '/' })
             } else {
               message.error('Такого пользователя не существует')
